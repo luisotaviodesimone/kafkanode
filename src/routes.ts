c852edd2f787bed8e61eafda7e19d22a1af86889
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-10-03 23:48:29 -0300

    ✨feat: apply middlewares and create producer route

--- src/routes.ts
+++ src/routes.ts
@@ -1,39 +1,62 @@
-import express from 'express';
-import { MyKafkaRequest, MyKafkaResponse } from './@types/api';
+import express, { NextFunction } from 'express';
+import { MyKafkaRequest, MyKafkaResponse } from './@types';
 import { processConsumer } from './consumer';
+import { processProducer } from './producer/producer';
+import {
+  ensureTopicMiddleware,
+  ensureTopicAndMessageMiddleware,
+} from './middlewares';
 
 const routes = express.Router();
 
-routes.post('/create-topic', async (req: MyKafkaRequest, res: MyKafkaResponse) => {
+routes.post(
+  '/create-topic',
+  ensureTopicMiddleware,
+  async (req: MyKafkaRequest, res: MyKafkaResponse) => {
+    const { admin } = req;
+    const { topic } = req.body;
 
-  const { admin } = req;
-  const { topic } = req.body;
+    await admin.connect();
 
-  await admin.connect();
+    await admin.createTopics({
+      topics: [
+        {
+          topic: topic,
+          numPartitions: 2,
+          replicationFactor: 1,
+        },
+      ],
+    });
 
-  await admin.createTopics({
-    topics: [
-      {
-        topic: topic,
-        numPartitions: 2,
-        replicationFactor: 1,
-      },
-    ],
-  });
+    await admin.disconnect();
 
-  await admin.disconnect();
+    return res.json({ response: 'Topic created' });
+  }
+);
 
-  return res.json({ message: 'Topic created' });
-});
+routes.post(
+  '/create-consumer',
+  async (req: MyKafkaRequest, res: MyKafkaResponse) => {
+    const { topic } = req.body;
 
-routes.post('/create-consumer', async (req: MyKafkaRequest, res: MyKafkaResponse) => {
-  const { topic } = req.body;
+    if (!topic) return res.json({ response: 'Topic not found' });
 
+    await processConsumer(topic);
 
-  await processConsumer(topic)
+    return res.json({ response: 'Consumer created' });
+  }
+);
 
-  return res.json({ message: 'Consumer created' });
+routes.post(
+  '/send-message',
+  ensureTopicAndMessageMiddleware,
+  async (req: MyKafkaRequest, res: MyKafkaResponse) => {
+    const { topic, message } = req.body;
 
-})
+    await processProducer(topic, message);
+
+    return res.json({ response: 'Message sent', topic, message });
+  }
+);
 
 export default routes;
